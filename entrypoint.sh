891c1b740ac99717c5b88a658c1404794e9bf4f5
odin <odinuv@gmail.com>
2019-09-04 09:48:22 +0200

    build

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,9 +17,8 @@ PUSH_LATEST=$7
 docker pull quay.io/keboola/developer-portal-cli-v2:latest
 
 echo "B"
-echo $TAG | /usr/bin/pcregrep -o '^(v?[0-9]+.[0-9]+.[0-9]+)'\$
 echo "A"
-export TARGET_TAG=`echo $TAG | /usr/bin/pcregrep -o '^(v?[0-9]+.[0-9]+.[0-9]+)'\$`
+export TARGET_TAG=`echo $TAG | /usr/bin/pcregrep -o '^(v?[0-9]+.[0-9]+.[0-9]+)$'`
 echo "TT: $TARGET_TAG"
 if [ $TARGET_TAG -eq "" ]
 then 
